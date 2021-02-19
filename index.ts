6ae8082da485ffd08a53b7044678d188cc334102
Charles-Hebert Dalzon <charlby5@gmail.com>
2021-02-18 20:47:39 -0500

    Execute first typescript code

---
+++ index.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+axios.get(url).then(res => {
+  console.log(res.data)
+}).catch(err => console.log(err))
